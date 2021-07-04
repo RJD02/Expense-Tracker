14d7f0bf5f989777c821c1c341840c441a62f11a
Raviraj Dulange <redhood.dumb@gmail.com>
2021-07-05 02:43:26 +0530

    Clean up the code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import ExpenseItem from "./components/ExpenseItem";
 import Expenses from "./components/Expenses";
 
 function App() {
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -3,7 +3,7 @@ import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 export default function Expenses(props) {
-    const expenses = props.expenses;
+  const expenses = props.expenses;
   return (
     <div className="expenses">
       <ExpenseItem
