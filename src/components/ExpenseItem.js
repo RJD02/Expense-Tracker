5ffd75aa7f9f7161cd872820615d32c0c7f49a5b
Raviraj Dulange <redhood.dumb@gmail.com>
2021-07-04 22:00:41 +0530

    Add css to the JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import './ExpenseItem.css'
 
 export default function ExpenseItem() {
-    return (
-        <div>
-            <h2>Expense Item!</h2>
-        </div>
-    )
+  return (
+    <div className='expense-item'>
+      <div>July 4th 2021</div>
+      <div className="expense-item__description">
+          <h2>Car Insurance</h2>
+          <div className="expense-item__price">$294.67</div>
+      </div>
+    </div>
+  );
 }
