5bb422139e3e5c329b7a92e4139c11959d219add
Raviraj Dulange <redhood.dumb@gmail.com>
2021-07-06 20:54:37 +0530

    Move the Expenses logic to another component and add styles to the component and return list, modify ExpenseItem for semantically correct code

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesList from "./ExpensesList";
 import "./Expenses.css";
 
 export default function Expenses(props) {
@@ -12,26 +13,14 @@ export default function Expenses(props) {
   const filteredExpenses = props.expenses.filter(
     (expense) => expense.date.getFullYear() === parseInt(filteredYear)
   );
-  let expenseContent = <p>No content found</p>;
-  if (filteredExpenses.length > 0) {
-    expenseContent = filteredExpenses.map((expense) => {
-      return (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      );
-    });
-  }
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         onYearChange={yearChangeHandler}
         selectedYear={filteredYear}
       />
-      {expenseContent}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 }
