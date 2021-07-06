d05a65f4a3ca48bd687bb1ae2df4cee6b5fcc1f0
Raviraj Dulange <redhood.dumb@gmail.com>
2021-07-06 20:29:21 +0530

    Render filtered list(end)

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,35 +6,19 @@ import "./Expenses.css";
 
 export default function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2021");
-  const [filteredYearList, setFilteredYearList] = useState([]);
   const yearChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-    setFilteredYearList(
-      props.expenses.filter(
-        (expense) => expense.date.getFullYear() === parseInt(selectedYear)
-      )
-    );
-    console.log("Inside Expenses.js");
-    console.log(selectedYear);
-    console.log(filteredYearList);
   };
+  const filterExpenses = props.expenses.filter(
+    (expense) => expense.date.getFullYear() === parseInt(filteredYear)
+  );
   return (
     <Card className="expenses">
       <ExpensesFilter
         onYearChange={yearChangeHandler}
         selectedYear={filteredYear}
       />
-      {props.expenses.map((expense) => {
-        return (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        );
-      })}
-      {filteredYearList.map((expense) => {
+      {filterExpenses.map((expense) => {
         return (
           <ExpenseItem
             key={expense.id}
