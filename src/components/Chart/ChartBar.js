bea22d51cf3251c2dd28192af471a9decb26d3ee
Raviraj Dulange <redhood.dumb@gmail.com>
2021-07-09 21:28:00 +0530

    Fix typo in ChartBar component for chart-bar__fill class

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -8,7 +8,10 @@ export default function ChartBar(props) {
   return (
     <div className="chart-bar">
       <div className="chart-bar__inner">
-        <div className="char-bar__fill" style={{ height: barFillHeight }}></div>
+        <div
+          className="chart-bar__fill"
+          style={{ height: barFillHeight }}
+        ></div>
       </div>
       <div className="chart-bar__label">{props.label}</div>
     </div>
